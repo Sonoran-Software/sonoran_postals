cb9437d8ba69262d20c94485de076f7da0a3ee31
Nathan C <me@nathanc.tech>
2020-05-13 15:01:36 -0400

    feature: server-side postal fetch

--- postals/sv_postals.lua
+++ postals/sv_postals.lua
@@ -7,7 +7,25 @@
 ]]
 
 -- Toggles Postal Sender
+
+PostalsCache = {}
+
 RegisterNetEvent("getShouldSendPostal")
 AddEventHandler("getShouldSendPostal", function()
     TriggerClientEvent("getShouldSendPostalResponse", source, prefixPostal)
 end)
+
+RegisterNetEvent("cadClientPostal")
+AddEventHandler("cadClientPostal", function(postal)
+    PostalsCache[source] = postal
+end)
+
+AddEventHandler("playerDropped", function(player)
+    PostalsCache[player] = nil
+end)
+
+function getNearestPostal(player)
+    return PostalsCache[player]
+end
+
+exports('cadGetNearestPostal', getNearestPostal)
